524e92e01cb94fa4ef1efcf29ea02ccf64ec634a
Bryan Chacha <morocho.12chacha@gmail.com>
2022-10-27 15:57:50 -0500

    Trabajando en pagina Que es el diseño web

--- _build/jupyter_execute/markdown-notebooks.py
+++ _build/jupyter_execute/markdown-notebooks.py
@@ -1,43 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Notebooks with MyST Markdown
+# # ¿Qué es el diseño web?
+# <img src="https://images.pexels.com/photos/196645/pexels-photo-196645.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+# <h2>
+#     El diseño web es la actividad que consiste en:
+# </h2>
+# <ul>
+#     <li style="color:#67686A;">Planificación de páginas webs.</li>
+#     <li style="color:#67686A;">Diseño de páginas webs.</li>
+#     <li style="color:#67686A;">Mantenimiento de páginas webs.</li>
+#     <li style="color:#67686A;">Creación de páginas webs.</li>
+#     <li style="color:#67686A;">Diseño de la interfaz de las páginas webs.</li>
+#     <li style="color:#67686A;">Diseño del material gráfico.</li>
+#     <li style="color:#67686A;">Experiencia del usuario en el sitio web.</li>
+# </ul>
+# <p style="color:#67686A; font-weight:bold; text-align:justify;">
+#     El diseño web consiste en planificación, diseño e implementación de sitios web. Dentro del diseño web también se abarcan temas como el diseño gráfico, diseño de interfaz, experiencia del usuario, la navegabilidad, interactividad, usabilidad, arquitectura de la información, etc..
+# </p>
 # 
-# Jupyter Book also lets you write text-based notebooks using MyST Markdown.
-# See [the Notebooks with MyST Markdown documentation](https://jupyterbook.org/file-types/myst-notebooks.html) for more detailed instructions.
-# This page shows off a notebook written in MyST Markdown.
+# <div style="display:flex;">
+#     <div><img src="https://media.istockphoto.com/photos/website-designer-creative-planning-application-developer-development-picture-id1206638576?b=1&k=20&m=1206638576&s=612x612&w=0&h=wPA8RbwU-lVzESg7O9IXbxpoXClItDDsTDEpkrhXzs0="/></div>
+#     <div><img src="https://media.istockphoto.com/photos/designers-drawing-website-ux-app-development-picture-id1196702694?b=1&k=20&m=1196702694&s=612x612&w=0&h=hXgW07jMeHxVl_csoP9QbNociVY5iLALDmAfZBU-JS4=" /></div>
+#     <div><img src="https://media.istockphoto.com/photos/ux-graphic-designer-creative-sketch-planning-application-process-picture-id1169087529?b=1&k=20&m=1169087529&s=612x612&w=0&h=PA0UMs9JiVGAuVCEYyLAKkHLWkonPC5DMyq8dvKFZEw=" /></div>
+#     <div><img src="https://media.istockphoto.com/photos/top-view-asian-ux-developer-and-ui-designer-brainstorming-about-app-picture-id1218511457?b=1&k=20&m=1218511457&s=612x612&w=0&h=VZ4UOZtqnu8HpM3Yj08G_GbvyukbV5OdQ-jGRn5SCzE=" /></div>
+#     
+# </div>
 # 
-# ## An example cell
-# 
-# With MyST Markdown, you can define code cells with a directive like so:
-
-# In[1]:
-
-
-print(2 + 2)
-
-
-# When your book is built, the contents of any `{code-cell}` blocks will be
-# executed with your default Jupyter kernel, and their outputs will be displayed
-# in-line with the rest of your content.
-# 
-# ```{seealso}
-# Jupyter Book uses [Jupytext](https://jupytext.readthedocs.io/en/latest/) to convert text-based files to notebooks, and can support [many other text-based notebook files](https://jupyterbook.org/file-types/jupytext.html).
-# ```
-# 
-# ## Create a notebook with MyST Markdown
-# 
-# MyST Markdown notebooks are defined by two things:
-# 
-# 1. YAML metadata that is needed to understand if / how it should convert text files to notebooks (including information about the kernel needed).
-#    See the YAML at the top of this page for example.
-# 2. The presence of `{code-cell}` directives, which will be executed with your book.
-# 
-# That's all that is needed to get started!
-# 
-# ## Quickly add YAML metadata for MyST Notebooks
-# 
-# If you have a markdown file and you'd like to quickly add YAML metadata to it, so that Jupyter Book will treat it as a MyST Markdown Notebook, run the following command:
 # 
 # ```
 # jupyter-book myst init path/to/markdownfile.md
